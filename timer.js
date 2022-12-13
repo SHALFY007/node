3e3f0f6eb3b72d61729381156323ac7db1eeb98e
vladislav <vl_evlashin@mail.ru>
2022-12-13 18:44:08 +0300

    Сделал второе дз

--- timer.js
+++ timer.js
@@ -3,10 +3,30 @@ const EventEmitter = require('events')
 class TimerEmitter extends EventEmitter {}
 const emitter = new TimerEmitter();
 
-const dateNow = new Date();
+const getPrettyTime = (seconds) => {
+    const values = [
+        Math.floor(seconds%60),
+        Math.floor((seconds /60) %60),
+        Math.floor(seconds/ (60*60) % 24),
+        Math.floor(seconds / (60*60*24)),
 
-emitter.on('timerTick', (dateInFuture, dateNow) => {
+    ]
+    return `${values.pop()} days, ${values.pop()} hours, ${values.pop()} minutes,  ${values.pop()} seconds`
+}
 
+emitter.on('timerTick', ([dateInFuture, timer]) => {
+    const dateNow = new Date();
+
+    if (!(dateNow <= dateInFuture)) {
+        emitter.emit('timerEnd', timer)
+    } else {
+        console.log(getPrettyTime((dateInFuture- dateNow)/1000), 'left')
+    }
+})
+
+emitter.on('timerEnd', (timer) => {
+    console.log('Time is up!');
+    clearInterval(timer)
 })
 
 const start = (dateInFuture) => {
@@ -17,5 +37,5 @@ const start = (dateInFuture) => {
 }
 
 const [hours, day, month, year] = process.argv[2].split('-');
-
-console.log(dateNow)
+const dateInFuture = new Date(year, month-1, day, hours);
+start(dateInFuture);
